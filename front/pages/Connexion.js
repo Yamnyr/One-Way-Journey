e0e39b0e4f7e50b3a1c12faf94842fa7f1595f1c
Malice776 <marinerapin@me.com>
2025-04-08 15:36:59 +0200

    Fonts / Accueil / Connexion

--- front/pages/Connexion.js
+++ front/pages/Connexion.js
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
     button: {
-        backgroundColor: '#1e90ff',
+        backgroundColor: 'rgba(34, 186, 186, 0.6)',
         padding: 15,
         borderRadius: 10,
         alignItems: 'center',
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
         fontFamily: 'Orbitron',
     },
     link: {
-        color: '#1e90ff',
+        color: 'rgb(34, 186, 186)',
         textAlign: 'center',
         marginTop: 20,
     },
