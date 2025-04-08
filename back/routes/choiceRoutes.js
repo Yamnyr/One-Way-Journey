48a3240a0e7f9c76c34f13b061f6273c4c01b28a
Yamnyr <quentinwaroquet@gmail.com>
2025-04-08 11:58:09 +0200

    utilisation du middleware

--- back/routes/choiceRoutes.js
+++ back/routes/choiceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const choiceController = require('../controllers/choiceController');
+const {verifyToken, verifyRole} = require("../middleware/authMiddleware");
 
-router.post('/', choiceController.createChoice);
-router.get('/:scenarioId', choiceController.getScenarioChoices);
+router.post('/',verifyToken, verifyRole(['admin']), choiceController.createChoice);
+router.get('/:scenarioId',verifyToken, choiceController.getScenarioChoices);
 
 module.exports = router;
--- back/routes/scenarioRoutes.js
+++ back/routes/scenarioRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const scenarioController = require('../controllers/scenarioController');
+const {verifyToken, verifyRole} = require("../middleware/authMiddleware");
 
-router.post('/', scenarioController.createScenario);
-router.get('/', scenarioController.getAllScenarios);
+router.post('/', verifyToken, verifyRole(['admin']), scenarioController.createScenario);
+router.get('/', verifyToken, scenarioController.getAllScenarios);
 
 module.exports = router;
